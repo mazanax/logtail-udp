f05fa2bb51501c5413b911a7c892d7996cd4d5ef
Alexander Gorshkov <agorshkov@mznx.ru>
2022-10-26 10:06:42 +0100

    Improve codestyle

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"github.com/mazanax/logtail-udp/version"
 	"gopkg.in/robfig/cron.v2"
-	"io/ioutil"
+	"log"
 	"net"
 	"net/http"
 	"os"
@@ -14,6 +14,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"syscall"
 )
 
 type LogEntry struct {
@@ -27,8 +28,7 @@ func main() {
 
 	logtailToken := os.Getenv("LOGTAIL_TOKEN")
 	if logtailToken == "" {
-		fmt.Printf("Err: LOGTAIL_TOKEN required\n")
-		os.Exit(2)
+		log.Fatalf("Err: LOGTAIL_TOKEN required\n")
 	}
 
 	listenHost := os.Getenv("LISTEN_HOST")
@@ -37,8 +37,7 @@ func main() {
 	}
 
 	if net.ParseIP(listenHost) == nil {
-		fmt.Printf("Err: LISTEN_HOST invalid\n")
-		os.Exit(2)
+		log.Fatalf("Err: LISTEN_HOST invalid\n")
 	}
 
 	listenPortStr := os.Getenv("LISTEN_PORT")
@@ -47,12 +46,10 @@ func main() {
 	}
 	listenPort, err := strconv.Atoi(listenPortStr)
 	if err != nil {
-		fmt.Printf("Err: LISTEN_PORT invalid (%s)\n", err)
-		os.Exit(2)
+		log.Fatalf("Err: LISTEN_PORT invalid (%s)\n", err)
 	}
 	if listenPort <= 0 || listenPort > 65535 {
-		fmt.Printf("Err: LISTEN_PORT invalid\n")
-		os.Exit(2)
+		log.Fatalf("Err: LISTEN_PORT invalid\n")
 	}
 	cronInterval := os.Getenv("LOGTAIL_SEND_INTERVAL")
 	if cronInterval == "" {
@@ -64,35 +61,27 @@ func main() {
 	}
 	logsBatchSize, err := strconv.Atoi(logsBatchSizeStr)
 	if err != nil {
-		fmt.Printf("Err: LOGS_BATCH_SIZE invalid (%s)\n", err)
-		os.Exit(2)
+		log.Fatalf("Err: LOGS_BATCH_SIZE invalid (%s)\n", err)
 	}
 
 	ctx, cancelFunction := context.WithCancel(context.Background())
 	logsBuffer := make(chan LogEntry, logsBatchSize)
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		for range c {
-			fmt.Printf("Gracefully shuting down...")
-			cancelFunction()
-			sendBufferToLogtail(logtailToken, logsBuffer)
-			os.Exit(1)
-		}
-	}()
+	ctrlC := make(chan os.Signal, 1)
+	signal.Notify(ctrlC, syscall.SIGINT, syscall.SIGTERM)
 
 	wg := &sync.WaitGroup{}
-
 	fmt.Printf("Started cron job with interval = %s\n", cronInterval)
 	wg.Add(1)
 	go startCronJob(ctx, wg, logtailToken, logsBuffer, cronInterval)
 
 	fmt.Printf("Listening %s:%d\n", listenHost, listenPort)
-	wg.Add(1)
-	startListeningUDP(ctx, wg, listenHost, listenPort, logsBuffer)
+	go startListeningUDP(ctx, wg, listenHost, listenPort, logsBuffer)
 
+	<-ctrlC
+	cancelFunction()
 	wg.Wait()
+	sendBufferToLogtail(logtailToken, logsBuffer)
 	fmt.Println("Good bye!")
 }
 
@@ -113,7 +102,7 @@ func startCronJob(ctx context.Context, wg *sync.WaitGroup, logtailToken string, 
 		case <-ctx.Done():
 			c.Stop()
 			wg.Done()
-			fmt.Printf("Debug: c stopped\n")
+			fmt.Printf("Debug: stopped\n")
 			return
 		default:
 			// do nothing
@@ -125,7 +114,7 @@ func startListeningUDP(ctx context.Context, wg *sync.WaitGroup, host string, por
 	addr := &net.UDPAddr{IP: net.ParseIP(host), Port: port}
 	conn, err := net.ListenUDP("udp", addr)
 	if err != nil {
-		fmt.Printf("Err: Cannot listen %s:%d (%s)\n", host, port, err)
+		log.Fatalf("Err: Cannot listen %s:%d (%s)\n", host, port, err)
 	}
 	defer conn.Close()
 
@@ -133,27 +122,29 @@ func startListeningUDP(ctx context.Context, wg *sync.WaitGroup, host string, por
 	for {
 		select {
 		case <-ctx.Done():
-			fmt.Printf("Stop listening\n")
 			wg.Done()
+			fmt.Printf("Stop listening\n")
 			return
 		default:
-			n, remoteAddr, err := conn.ReadFromUDP(buffer)
-			if err != nil {
-				fmt.Printf("Err: %s\n", err)
-				continue
-			}
-
-			received := string(buffer[0:n])
-			fmt.Printf("Debug: received %s from %s\n", received, remoteAddr.String())
-			logsBuffer <- LogEntry{remoteAddr.String(), received}
+			// go forward
 		}
+
+		n, remoteAddr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Printf("Err: %s\n", err)
+			return
+		}
+
+		received := string(buffer[0:n])
+		fmt.Printf("Debug: received %s from %s\n", received, remoteAddr.String())
+		logsBuffer <- LogEntry{remoteAddr.String(), received}
 	}
 }
 
 func sendBufferToLogtail(logtailToken string, logsBuffer chan LogEntry) {
 	payload := readAll(logsBuffer)
 	fmt.Printf("Debug: have %d messages\n", len(payload))
-	if len(payload) < 3 {
+	if len(payload) == 0 {
 		return
 	}
 
@@ -177,10 +168,7 @@ func sendBufferToLogtail(logtailToken string, logsBuffer chan LogEntry) {
 		fmt.Printf("Err: logtail error (%s)\n", err)
 		return
 	}
-	defer response.Body.Close()
-
-	body, _ := ioutil.ReadAll(response.Body)
-	fmt.Printf("Response: Status = %s, Body = %s\n", response.Status, string(body))
+	fmt.Printf("Response: Status = %s\n", response.Status) // body always empty so we have to know only status
 }
 
 func readAll(logsBuffer chan LogEntry) []LogEntry {
--- version/version.go
+++ version/version.go
@@ -1,12 +1,21 @@
 package version
 
 import (
-	"fmt"
-	"strings"
+	"runtime/debug"
 )
 
 // Version returns actual version of application. Usually it's a git hash but in dev environment this function returns "dev"
 func Version() string {
-	originalString := "INJECT VERSION HERE"
-	return strings.ReplaceAll("<INJECT VERSION HERE>", fmt.Sprintf("<%s>", originalString), "dev")
+	b, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "dev"
+	}
+
+	for _, kv := range b.Settings {
+		if kv.Key == "vcs.revision" {
+			return kv.Value
+		}
+	}
+
+	return "dev"
 }
